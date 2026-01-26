7107ebfbbc6843659abc5b8d9c23f76e3ecb741b
AbhayPc <abhaypc112@gmail.com>
2026-01-26 14:46:55 +0530

    ci/cd 3rd

--- src/v1/services/auth/auth.service.ts
+++ src/v1/services/auth/auth.service.ts
@@ -22,8 +22,8 @@ export interface googleAuth {
 export const initiateAuth = async () => {
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile email`;
   return {
-    message: "success done",
-    data: "url",
+    message: "success",
+    data: url,
   };
 };
 
